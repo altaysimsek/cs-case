750d87b63927dd8c299fd47ea29ee52b00017669
Altay Simsek <altaysimsek16@gmail.com>
2022-09-04 16:45:47 +0300

    feat: adding item to the bucket

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,44 @@
-import ItemImage from '../../assets/img/items/item.png';
-const Card = () => {
+/* eslint-disable react/prop-types */
+import PropTypes from 'prop-types';
+import { useContext } from 'react';
+import { BucketContext } from '../../contexts/BucketContext';
+
+const Card = ({ detail }) => {
+  const { bucket, setBucket } = useContext(BucketContext);
+
+  const addToBucket = () => {
+    const newBucket = [...bucket];
+    const index = newBucket.findIndex((item) => item.id === detail.id);
+    if (index === -1) {
+      newBucket.push({ ...detail, count: 1 });
+    } else {
+      newBucket[index].count++;
+    }
+    setBucket(newBucket);
+  };
+
   return (
     <div className="csCard">
-      <img src={ItemImage} alt="example_Item"></img>
+      <img src={detail.image} alt="example_Item"></img>
       <div className="csCard__status">
-        <span className="csCard__status__title">Gold Çizgili Vazoda 2 Dal Orkide Çiçeği</span>
-        <span className="csCard__status__tag">Ücretsiz Teslimat</span>
-        <span className="csCard__status__price">249,90 TL</span>
+        <span className="csCard__status__title">{detail.name}</span>
+        {detail.freePackage && <span className="csCard__status__tag">Ücretsiz Teslimat</span>}
+        <span className="csCard__status__price">{parseFloat(detail.price)} TL</span>
       </div>
-      <button type="button" className="csCard__button">
+      <button type="button" onClick={addToBucket} className="csCard__button">
         {' '}
         Sepete Ekle
       </button>
     </div>
   );
+};
+
+Card.PropTypes = {
+  detail: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired
+  })
 };
 
 export default Card;
