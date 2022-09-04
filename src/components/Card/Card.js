cf4c0480af62b5ccef9d12945a42926f5e5d3b5f
Altay Simsek <altaysimsek16@gmail.com>
2022-09-04 17:44:14 +0300

    feat: remove and add to bucket buttons

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -17,6 +17,26 @@ const Card = ({ detail }) => {
     setBucket(newBucket);
   };
 
+  const removeFromBucket = () => {
+    const newBucket = [...bucket];
+    const index = newBucket.findIndex((item) => item.id === detail.id);
+    if (index !== -1) {
+      newBucket[index].count--;
+      if (newBucket[index].count === 0) {
+        newBucket.splice(index, 1);
+      }
+    }
+    setBucket(newBucket);
+  };
+
+  const calculateCount = () => {
+    const index = bucket.findIndex((item) => item.id === detail.id);
+    if (index === -1) {
+      return 0;
+    }
+    return bucket[index].count;
+  };
+
   return (
     <div className="csCard">
       <img src={detail.image} alt="example_Item"></img>
@@ -25,10 +45,22 @@ const Card = ({ detail }) => {
         {detail.freePackage && <span className="csCard__status__tag">Ücretsiz Teslimat</span>}
         <span className="csCard__status__price">{parseFloat(detail.price)} TL</span>
       </div>
-      <button type="button" onClick={addToBucket} className="csCard__button">
-        {' '}
-        Sepete Ekle
-      </button>
+      {calculateCount() > 0 ? (
+        <div className="csCard__buttons">
+          <button type="button" className="csCard__buttons__plus" onClick={addToBucket}>
+            +
+          </button>
+          <span>{calculateCount()}</span>
+          <button type="button" onClick={removeFromBucket} className="csCard__buttons__minus">
+            -
+          </button>
+        </div>
+      ) : (
+        <button type="button" onClick={addToBucket} className="csCard__button">
+          {' '}
+          Sepete Ekle
+        </button>
+      )}
     </div>
   );
 };
