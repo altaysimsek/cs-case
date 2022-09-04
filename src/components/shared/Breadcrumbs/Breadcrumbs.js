cc12d7672e07d6f66fcc9a8534926934b39392e4
Altay Simsek <altaysimsek16@gmail.com>
2022-09-04 22:01:01 +0300

    feat: breadcrumbs dynamic title

--- src/components/shared/Breadcrumbs/Breadcrumbs.js
+++ src/components/shared/Breadcrumbs/Breadcrumbs.js
@@ -1,9 +1,12 @@
+import { useContext } from 'react';
+import { BucketContext } from '../../../contexts/BucketContext';
 const Breadcrumbs = () => {
+  const { activeCategory } = useContext(BucketContext);
   return (
     <div className="csBreadCrumb container">
       {/* TODO: Make reachable with link  */}
       CicekSepeti Market {' > '} Istanbul {' > '}
-      <span className="csBreadCrumb__link--active">CiceksepetiBreadCrumb</span>
+      <span className="csBreadCrumb__link--active">{activeCategory.name}</span>
     </div>
   );
 };
